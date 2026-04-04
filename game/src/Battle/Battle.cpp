790ccf2beb147e6385e41ea129d4394c61bc515d
Cameron Przybylski <cameronprzybylski@yahoo.com>
2026-04-04 15:41:44 -0500

    LoadGame add weapon before equipping

--- game/src/Battle/Battle.cpp
+++ game/src/Battle/Battle.cpp
@@ -574,6 +574,7 @@ void Battle::LoadGame()
             {
                 std::string weaponKey = weapon.key();
                 int weaponDamage = weapon.value();
+                player->AddWeapon(weapon.key(), std::make_shared<Weapon>(weapon.value(), weapon.key()));
                 player->ChangeWeapon(weaponKey);
             }
             for(auto weapon : item.value()["weapons"].items())
